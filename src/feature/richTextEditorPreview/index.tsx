ab179a91ef6e629a969fc7224909d70c4d4523f2
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-06 17:16:22 +0900

    プレビューdom表示

--- src/feature/richTextEditorPreview/index.tsx
+++ src/feature/richTextEditorPreview/index.tsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { previewData } from '@/feature/richTextEditorPreview/types/index'
 
 export const RichTextEditorPreview = (props: previewData) => {
 
 	const { data } = props
 	const [previewData, setPreviewData] = useState(data ?? '')
+
+	useEffect(() => {
+		setPreviewData(data)
+	}, [data])
+
 	return (
 		<>
 			<h2 className="c_h2" style={{ marginTop: '32px' }}>プレビュー</h2>
 			<div className="quill">
-				{ previewData }
+				<div
+					className='ql-editor'
+					dangerouslySetInnerHTML={{__html: previewData}}
+					style={{ padding: '0' }}
+				/>
 			</div>
 		</>
 	)
