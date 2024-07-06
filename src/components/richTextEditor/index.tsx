cb35c7a995b41d53782171493b2be96ae369185b
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-06 16:31:49 +0900

    react-quillをSSRに対応させる

--- src/components/richTextEditor/index.tsx
+++ src/components/richTextEditor/index.tsx
@@ -1,20 +1,41 @@
 'use client'
 
-import { useState } from 'react'
-import ReactQuill from 'react-quill';
+import { useState, useRef, LegacyRef } from 'react'
+import dynamic from 'next/dynamic'
+import type ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { Data } from '@/components/richTextEditor/types/index'
 
+interface IWrappedComponent extends React.ComponentProps<typeof ReactQuill> {
+  forwardedRef: LegacyRef<ReactQuill>
+}
+
+const ReactQuillBase = dynamic(
+  async () => {
+    const {default: RQ} = await import('react-quill')
+
+    function QuillJS({forwardedRef, ...props}: IWrappedComponent) {
+      return <RQ ref={forwardedRef} {...props} />
+    }
+
+    return QuillJS
+  },
+  {
+    ssr: false,
+  },
+)
 
 export const RichTextEditor = (props: Data) => {
 
   const { valueData } = props
   const [value, setValue] = useState(valueData ?? '')
+  const quillRef = useRef<ReactQuill>(null)
 
   return (
     <>
-      <ReactQuill
-        theme="snow"
+      <ReactQuillBase
+        forwardedRef={quillRef}
+        theme="snow" 
         modules={{
           toolbar: [
             [{ 'size': ['small', false, 'large', 'huge'] }],
