d7a0208fbdd17ca198f99b189462e2ac7c276038
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-06 17:00:39 +0900

    preview読み込み

--- src/components/richTextEditor/index.tsx
+++ src/components/richTextEditor/index.tsx
@@ -5,6 +5,7 @@ import dynamic from 'next/dynamic'
 import type ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { Data } from '@/components/richTextEditor/types/index'
+import RichTextEditorPreview from '@/feature/richTextEditorPreview';
 
 interface IWrappedComponent extends React.ComponentProps<typeof ReactQuill> {
   forwardedRef: LegacyRef<ReactQuill>
@@ -32,24 +33,29 @@ export const RichTextEditor = (props: Data) => {
   const quillRef = useRef<ReactQuill>(null)
 
   return (
-    <>
-      <ReactQuillBase
-        forwardedRef={quillRef}
-        theme="snow" 
-        modules={{
-          toolbar: [
-            [{ 'size': ['small', false, 'large', 'huge'] }],
-            [ 'bold', 'underline' ],
-            [{ 'list': 'bullet'}],
-            [{ 'color': [] }],
-            ['link'],
-          ]
-        }}
-        placeholder='5000文字以内で記載してください。'
-        value={ value }
-        onChange={setValue}
-      />
-    </>
+    <div className='c_richtext_editor'>
+      <div className='c_richtext_editor_quill'>
+        <ReactQuillBase
+          forwardedRef={quillRef}
+          theme="snow" 
+          modules={{
+            toolbar: [
+              [{ 'size': ['small', false, 'large', 'huge'] }],
+              [ 'bold', 'underline' ],
+              [{ 'list': 'bullet'}],
+              [{ 'color': [] }],
+              ['link'],
+            ]
+          }}
+          placeholder='5000文字以内で記載してください。'
+          value={ value }
+          onChange={setValue}
+        />
+      </div>
+      <div className='c_richtext_editor_preview'>
+        <RichTextEditorPreview data={value} />
+      </div>
+    </div>
   )
 }
 
